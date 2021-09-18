3fb34f099212da101f345fef6eadb1574341331a
Darko Draskovic <darko.draskovic@gmail.com>
2021-09-18 12:20:47 +0200

    Add Point URP shader
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -5,22 +5,27 @@ using UnityEngine;
 public class Graph : MonoBehaviour
 {
     [SerializeField]
-    Transform pointPrefab;
+    GameObject pointPrefab;
     [SerializeField, Range(0, 50)]
-    int resolution = 10;
+    int resolution = 32;
+    GameObject[] points;
 
     void Awake()
     {
         float step = 1f / resolution;
-        var position = Vector3.zero;
-        var scale = Vector3.one * step;
+        Vector3 position = Vector3.zero;
+        Vector3 scale = Vector3.one * step;
+
+        points = new GameObject[resolution];
+
         for (int i = 0; i < resolution; i++)
         {
-            Transform point = Instantiate(pointPrefab);
-            position.x = i * step - .5f;
-            position.y = position.x * position.x;
-            point.localScale = Vector3.one * step;
-            point.localPosition = position;
+            GameObject pointObject = points[i] = Instantiate<GameObject>(pointPrefab);
+            position.x = (i * step - .5f) * 2;
+            // position.y = Mathf.Pow(position.x, 3) * 4;
+            pointObject.transform.localScale = scale;
+            pointObject.transform.localPosition = position;
+            pointObject.transform.SetParent(transform, false);
         }
     }
 
@@ -32,6 +37,14 @@ public class Graph : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform pointTransform = points[i].transform;
+            Vector3 position = pointTransform.localPosition;
+            // position.y = Mathf.Pow(position.x, 3);
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            pointTransform.localPosition = position;
+        }
     }
 }
---
+++ Assets/Shaders/Point.shader
@@ -0,0 +1,26 @@
+Shader "Graph/Point" {
+    Properties {
+        _Smoothness ("Smoothness", Range(0,1)) = 0.5
+    }
+
+    SubShader {
+
+        CGPROGRAM
+            #pragma surface ConfigureSurface Standard fullforwardshadows
+            #pragma target 3.0
+
+            float _Smoothness;
+
+            struct Input {
+                float3 worldPos;
+            };
+
+            void ConfigureSurface (Input input, inout SurfaceOutputStandard surface) {
+                surface.Albedo.rg = input.worldPos.xy * .5 + .5;
+                surface.Smoothness = _Smoothness;
+            }
+        ENDCG
+    }
+
+    Fallback "Diffuse"
+}
