237f913500f6a334e6c2996c2c8225c9bc064cb5
Darko Draskovic <darko.draskovic@gmail.com>
2021-09-19 10:10:44 +0200

    Use z to add square of points in Graph example
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -6,8 +6,10 @@ public class Graph : MonoBehaviour
 {
     [SerializeField]
     GameObject pointPrefab;
-    [SerializeField, Range(0, 50)]
+    [SerializeField, Range(0, 64)]
     int resolution = 32;
+    [SerializeField]
+    FunctionLibrary.FunctionName function;
     GameObject[] points;
 
     void Awake()
@@ -16,12 +18,21 @@ public class Graph : MonoBehaviour
         Vector3 position = Vector3.zero;
         Vector3 scale = Vector3.one * step;
 
-        points = new GameObject[resolution];
+        points = new GameObject[resolution * resolution];
 
-        for (int i = 0; i < resolution; i++)
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
+            // Each time we finish a row we have to reset x back to zero
+            if (x == resolution)
+            {
+                x = 0;
+                // Increment z when we move on to the next row
+                z++;
+            }
+
             GameObject pointObject = points[i] = Instantiate<GameObject>(pointPrefab);
-            position.x = (i * step - .5f) * 2;
+            position.x = (x * step - .5f) * 2;
+            position.z = (z * step - .5f) * 2;
             // position.y = Mathf.Pow(position.x, 3) * 4;
             pointObject.transform.localScale = scale;
             pointObject.transform.localPosition = position;
@@ -37,13 +48,13 @@ public class Graph : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
         float time = Time.time;
         for (int i = 0; i < points.Length; i++)
         {
             Transform pointTransform = points[i].transform;
             Vector3 position = pointTransform.localPosition;
-            // position.y = Mathf.Pow(position.x, 3);
-            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            position.y = f(position.x, position.z, Time.time);
             pointTransform.localPosition = position;
         }
     }
